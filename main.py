bf89ce97e2ca1f9e2057057915dd39a839fa20ed
MishanyaS <shilinm1203@gmail.com>
2024-12-19 14:47:18 +0200

    Add saving data in csv format

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from parser import get_books_data, write_to_excel, write_to_db
+from parser import get_books_data, write_to_excel, write_to_db, write_to_csv
 
 if __name__ == '__main__':
     write_to_excel(get_books_data)
     write_to_db(get_books_data)
+    write_to_csv()
